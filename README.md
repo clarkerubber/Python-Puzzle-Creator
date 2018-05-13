d856abec895fa4eab3dc0f9376a2d5f463f6123d
forhavu <24998566+forhavu@users.noreply.github.com>
2018-05-13 05:12:00 -0700

    Adapt to run locally, and output a bit more info on --quiet

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About
 
-This is a python application dedicated to creating chess puzzles for lichess.org. However it can easily be adapted to create puzzles from personal games.
+This is a python application dedicated to creating chess puzzles locally for your personal games.
 
 ## Installation
 
@@ -22,7 +22,9 @@ MacOS / Linux : `sh build-stockfish.sh` to obtain the current lichess Stockfish 
 
 ## Launching Application
 
-`python main.py <Secret API Token> <#Threads = 4> <Hash (MBytes) = 2048>`
+`python main.py <filepath to pgn> <#Threads = 4> <Hash (MBytes) = 2048> --quiet`
 
 If your system has systemd, you can use the service file provided, it is tuned for a local virtualenv in a dedicated user account. If you run it as root with system-wide pip packages, use `ExecStart=/usr/bin/python main.py --quiet $key $instance $threads`
+
+JSON puzzle output will be in the file `generated-puzzles` in the `Python-Puzzle-Creator` directory
 
--- main.py
+++ main.py
@@ -13,11 +13,11 @@ from modules.fishnet.fishnet import stockfish_command
 from modules.puzzle.puzzle import puzzle
 from modules.bcolors.bcolors import bcolors
 from modules.investigate.investigate import investigate
-from modules.api.api import get_pgn, post_puzzle
+from modules.api.api import post_puzzle
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("token", metavar="TOKEN",
-                    help="secret token for the lichess api")
+parser.add_argument("pgn", metavar="PGN",
+                    help="filepath to the pgn file you wish to search")
 parser.add_argument("threads", metavar="THREADS", nargs="?", type=int, default=4,
                     help="number of engine threads")
 parser.add_argument("memory", metavar="MEMORY", nargs="?", type=int, default=2048,
@@ -38,8 +38,8 @@ except ImportError:
     pass
 
 logging.basicConfig(format="%(message)s", level=settings.loglevel, stream=sys.stdout)
-logging.getLogger("requests.packages.urllib3").setLevel(logging.WARNING)
-logging.getLogger("chess.uci").setLevel(logging.WARNING)
+# logging.getLogger("requests.packages.urllib3").setLevel(logging.WARNING)
+# logging.getLogger("chess.uci").setLevel(logging.DEBUG)
 
 engine = chess.uci.popen_engine(stockfish_command())
 engine.setoption({'Threads': settings.threads, 'Hash': settings.memory})
@@ -47,42 +47,41 @@ engine.uci()
 info_handler = chess.uci.InfoHandler()
 engine.info_handlers.append(info_handler)
 
-while True:
-    pgn = get_pgn(settings.token)
-    game = chess.pgn.read_game(pgn)
-    pgn.close()
+with open(settings.pgn) as pgn:
+    while True:
+        game = chess.pgn.read_game(pgn)
 
-    node = game
+        node = game
 
-    game_id = game.headers["Site"].split('/')[-1:][0]
-    logging.debug(bcolors.WARNING + "Game ID: " + game_id + bcolors.ENDC)
+        game_id = game.headers["Site"].split('/')[-1:][0]
+        logging.info(bcolors.WARNING + "Game ID: " + game_id + bcolors.ENDC)
 
-    prev_score = chess.uci.Score(None, None)
-    puzzles = []
+        prev_score = chess.uci.Score(None, None)
+        puzzles = []
 
-    logging.debug(bcolors.OKGREEN + "Game Length: " + str(game.end().board().fullmove_number))
-    logging.debug("Analysing Game..." + bcolors.ENDC)
+        logging.info(bcolors.OKGREEN + "Game Length: " + str(game.end().board().fullmove_number))
+        logging.info("Analysing Game..." + bcolors.ENDC)
 
-    engine.ucinewgame()
+        engine.ucinewgame()
 
-    while not node.is_end():
-        next_node = node.variation(0)
-        engine.position(next_node.board())
+        while not node.is_end():
+            next_node = node.variation(0)
+            engine.position(next_node.board())
 
-        engine.go(nodes=3500000)
-        cur_score = info_handler.info["score"][1]
-        logging.debug(bcolors.OKGREEN + node.board().san(next_node.move) + bcolors.ENDC)
-        logging.debug(bcolors.OKBLUE + "   CP: " + str(cur_score.cp))
-        logging.debug("   Mate: " + str(cur_score.mate) + bcolors.ENDC)
-        if investigate(prev_score, cur_score, node.board()):
-            logging.debug(bcolors.WARNING + "   Investigate!" + bcolors.ENDC)
-            puzzles.append(puzzle(node.board(), next_node.move, game_id, engine, info_handler))
+            engine.go(nodes=3500000)
+            cur_score = info_handler.info["score"][1]
+            logging.debug(bcolors.OKGREEN + node.board().san(next_node.move) + bcolors.ENDC)
+            logging.debug(bcolors.OKBLUE + "   CP: " + str(cur_score.cp))
+            logging.debug("   Mate: " + str(cur_score.mate) + bcolors.ENDC)
+            if investigate(prev_score, cur_score, node.board()):
+                logging.debug(bcolors.WARNING + "   Investigate!" + bcolors.ENDC)
+                puzzles.append(puzzle(node.board(), next_node.move, game_id, engine, info_handler))
 
-        prev_score = cur_score
-        node = next_node
+            prev_score = cur_score
+            node = next_node
 
-    for i in puzzles:
-        logging.debug(bcolors.WARNING + "Generating new puzzle..." + bcolors.ENDC)
-        i.generate()
-        if i.is_complete():
-            post_puzzle(settings.token, i)
+        for i in puzzles:
+            logging.info(bcolors.WARNING + "Generating new puzzle..." + bcolors.ENDC)
+            i.generate()
+            if i.is_complete():
+                post_puzzle(i)
